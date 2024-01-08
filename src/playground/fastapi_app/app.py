434a65482aa05fbb3e6c7857a89496e6dcd3ef0b
Marcel Johannesmann <mj0nez@fn.de>
2024-01-08 22:03:01 +0100

    chore: some customization tests

--- src/playground/fastapi_app/app.py
+++ src/playground/fastapi_app/app.py
@@ -1,11 +1,8 @@
-import uvicorn
 from fastapi import FastAPI
 
-from playground.fastapi_app.routes import users_router
+from playground.fastapi_app.routes import interactions_router, users_router
 
-app = FastAPI()
-
+app = FastAPI(debug=True)
 
 app.include_router(users_router)
-
-
+app.include_router(interactions_router)
